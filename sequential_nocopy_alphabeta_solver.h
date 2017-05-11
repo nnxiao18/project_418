39237f9e316a46076cd3f1c0d0807cdf04154f78
Jemmin Chang <jchang504@gmail.com>
2017-05-11 00:10:53 -0400

    Fix correctness of no-copy minimax and alpha-beta
    To fix it most quickly, I just copied the top level call into a separate,
    non-recursive function that actually plays the move, whereas the main recursive
    function just evaluates a game state (undoing any moves applied). A lot of
    duplicated code but it actually makes things easier to work with and should
    work well for applying OpenMP parallelism to the top level call.

--- sequential_nocopy_alphabeta_solver.h
+++ sequential_nocopy_alphabeta_solver.h
@@ -17,6 +17,8 @@ class SequentialNoCopyAlphaBetaSolver : public GameSolver<Game> {
     // bounds up to plies, plays it, and returns its minimax value.
     int playBestMoveForGame(Game& game, bool first_player_turn, int plies, int
             alpha, int beta);
+    int evalState(Game& game, bool first_player_turn, int plies, int alpha, int
+            beta);
   public:
     virtual void playBestMove();
 };
@@ -53,8 +55,8 @@ int SequentialNoCopyAlphaBetaSolver<Game, depth>::playBestMoveForGame(
                 if (plies == 0) {
                     score = game.leafEvalState();
                 } else {
-                    score = playBestMoveForGame(game, !first_player_turn,
-                            plies - 1, alpha, beta);
+                    score = evalState(game, !first_player_turn, plies - 1,
+                            alpha, beta);
                 }
                 break;
             case Game::kFirstPlayerWon:
@@ -96,6 +98,63 @@ int SequentialNoCopyAlphaBetaSolver<Game, depth>::playBestMoveForGame(
         game.undoMove(m);
     }
     game.playMove(best_move);
+    return best_score;
+}
+
+template <class Game, int depth>
+int SequentialNoCopyAlphaBetaSolver<Game, depth>::evalState(
+        Game& game, bool first_player_turn, int plies, int alpha, int beta) {
+    auto moves = game.availableMoves();
+    int best_score = first_player_turn ? INT_MIN : INT_MAX;
+    for (auto m : moves) {
+        game.playMove(m);
+        int score = 0;
+        switch (game.status()) {
+            case Game::kFirstPlayerTurn:
+            case Game::kSecondPlayerTurn:
+                if (plies == 0) {
+                    score = game.leafEvalState();
+                } else {
+                    score = evalState(game, !first_player_turn, plies - 1,
+                            alpha, beta);
+                }
+                break;
+            case Game::kFirstPlayerWon:
+                score = INT_MAX;
+                break;
+            case Game::kSecondPlayerWon:
+                score = INT_MIN;
+                break;
+            case Game::kTie:
+                // TODO: Does this make sense? A tie is of neutral value?
+                score = 0;
+                break;
+        }
+        if (first_player_turn) {
+            if (score > best_score) {
+                best_score = score;
+            }
+            if (score > alpha) {
+                alpha = score;
+                if (alpha >= beta) {
+                    game.undoMove(m);
+                    break;  // Prune
+                }
+            }
+        } else {
+            if (score < best_score) {
+                best_score = score;
+            }
+            if (score < beta) {
+                beta = score;
+                if (beta <= alpha) {
+                    game.undoMove(m);
+                    break;  // Prune
+                }
+            }
+        }
+        game.undoMove(m);
+    }
     return best_score;
 }
 
--- sequential_nocopy_minimax_solver.h
+++ sequential_nocopy_minimax_solver.h
@@ -16,6 +16,7 @@ class SequentialNoCopyMinimaxSolver : public GameSolver<Game> {
     // Finds the best move for game with minimax evaluation up to plies, plays
     // it, and returns its minimax value.
     int playBestMoveForGame(Game& game, bool first_player_turn, int plies);
+    int evalState(Game& game, bool first_player_turn, int plies);
   public:
     virtual void playBestMove();
 };
@@ -52,8 +53,7 @@ int SequentialNoCopyMinimaxSolver<Game, depth>::playBestMoveForGame(
                 if (plies == 0) {
                     score = game.leafEvalState();
                 } else {
-                    score = playBestMoveForGame(game, !first_player_turn,
-                            plies - 1);
+                    score = evalState(game, !first_player_turn, plies - 1);
                 }
                 break;
             case Game::kFirstPlayerWon:
@@ -85,6 +85,52 @@ int SequentialNoCopyMinimaxSolver<Game, depth>::playBestMoveForGame(
         game.undoMove(m);
     }
     game.playMove(best_move);
+    return best_score;
+}
+
+template <class Game, int depth>
+int SequentialNoCopyMinimaxSolver<Game, depth>::evalState(
+        Game& game, bool first_player_turn, int plies) {
+    auto moves = game.availableMoves();
+    int best_score = first_player_turn ? INT_MIN : INT_MAX;
+    for (auto m : moves) {
+        game.playMove(m);
+        int score = 0;
+        switch (game.status()) {
+            case Game::kFirstPlayerTurn:
+            case Game::kSecondPlayerTurn:
+                if (plies == 0) {
+                    score = game.leafEvalState();
+                } else {
+                    score = evalState(game, !first_player_turn, plies - 1);
+                }
+                break;
+            case Game::kFirstPlayerWon:
+                // TODO: If first_player_turn, could break out of for loop here
+                // because we know we can't do better than a winning move.
+                score = INT_MAX;
+                break;
+            case Game::kSecondPlayerWon:
+                // TODO: If !first_player_turn, could break out of for loop
+                // here because we know we can't do better than a winning move.
+                score = INT_MIN;
+                break;
+            case Game::kTie:
+                // TODO: Does this make sense? A tie is of neutral value?
+                score = 0;
+                break;
+        }
+        if (first_player_turn) {
+            if (score > best_score) {
+                best_score = score;
+            }
+        } else {
+            if (score < best_score) {
+                best_score = score;
+            }
+        }
+        game.undoMove(m);
+    }
     return best_score;
 }
 
